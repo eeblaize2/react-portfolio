5e36971efa481bfe5e45420c0e3b100d6202945f
Erin Blaize <eeblaize2@hotmail.com>
2021-07-14 19:35:28 -0700

    added an import for the about me picture

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Me from '../images/profile_IMG_5989.png'
 
 const About = () => {
     return (
         <div className= 'About'>
-           <img src= '/profile_IMG_5989.png' alt= 'Erin'/>
+           <img src= {Me} alt= 'Erin'/>
            <div className= 'About-Details'>
                <h2>Full Stack Developer</h2>
                <p>Currently studying full-stack programming through the University of Washington</p>
